c26f9a944e7653ed7858eeb3700c609768809512
ndaemy <yuyaebean@gmail.com>
2021-03-07 00:35:57 +0900

    Feat: If number of applicants changes, send message

--- src/slack/addApplicantsEvent.ts
+++ src/slack/addApplicantsEvent.ts
@@ -5,7 +5,16 @@ import { getApplicantsInfo } from "../selenium";
 function addApplicantsEvent(app: App) {
   app.message("지원자 수", async ({ message, say }) => {
     console.log(message);
-    await say(getApplicantsInfo().numberOfApplicantsString);
+    let text = getApplicantsInfo().numberOfApplicantsString;
+    await say(text);
+
+    setInterval(async () => {
+      const newText = getApplicantsInfo().numberOfApplicantsString;
+      if (text !== newText) {
+        text = newText;
+        await say(newText);
+      }
+    }, 180000);
   });
 }
 

5e4a6367ee307de35adab2247767365688ba43eb
Yebin Yu <yuyaebean@gmail.com>
2021-04-03 23:45:36 +0900

    Feat: Add 고라니 배틀

--- src/slack/addBasicEvent.ts
+++ src/slack/addBasicEvent.ts
@@ -37,6 +37,26 @@ function addBasicEvent(app: App) {
     } = await client.users.info({ user: message.user });
     await say(`${real_name}: ${Math.ceil(Math.random() * 100)} 나왔습니다.`);
   });
+
+  app.message("고라니 배틀", async ({client, message, say}) => {
+    // @ts-expect-error
+    const opponent = message.text.split('고라니 배틀 ')[1];
+
+    const {
+      // @ts-expect-error
+      user: {real_name},
+      // @ts-expect-error
+    } = await client.users.info({user: message.user})
+
+    const winner = Math.floor(Math.random() * 2);
+
+    if (winner) {
+      // if winner equals 1
+      await say(`${opponent} 물에 빠져 우적댑니다! 승자는 ${real_name}!`)
+    } else {
+      await say(`${opponent} 독가스가 살포되었지만 방독면이 있었습니다! ${real_name} 패배!`)
+    }
+  })
 }
 
 export default addBasicEvent;

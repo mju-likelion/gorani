33b47d11de560cf3c8386a6332c684e3ef6f0bc9
ndaemy <yuyaebean@gmail.com>
2021-04-19 23:10:18 +0900

    Feat: Add gorani birthday

--- src/slack/addBasicEvent.ts
+++ src/slack/addBasicEvent.ts
@@ -1,9 +1,9 @@
-import { App } from "@slack/bolt";
+import {App} from "@slack/bolt";
 
 import randomMessage from "../lib/randomMessage";
 
 function addBasicEvent(app: App) {
-  app.event("app_home_opened", async ({ event, client }) => {
+  app.event("app_home_opened", async ({event, client}) => {
     try {
       await client.views.publish({
         user_id: event.user,
@@ -27,34 +27,55 @@ function addBasicEvent(app: App) {
     }
   });
 
-  app.message(/[Gg]orani [Cc]ry|고라니 울어/, async ({ say }) => {
+  app.message(/[Gg]orani [Cc]ry|고라니 울어/, async ({say}) => {
     await say("키야아아아악! 아아아아아악!");
   });
 
-  app.message(/[Gg]orani [Tt]hrow|고라니 던져/, async ({ client, message, say }) => {
+  app.message(/[Gg]orani [Tt]hrow|고라니 던져/, async ({client, message, say}) => {
     const {
       // @ts-expect-error
-      user: { real_name },
+      user: {real_name},
       // @ts-expect-error
-    } = await client.users.info({ user: message.user });
+    } = await client.users.info({user: message.user});
     await say(`${real_name}: ${Math.ceil(Math.random() * 100)} 나왔습니다.`);
   });
 
-  app.message(/[Gg]orani [Bb]attle|고라니 배틀/, async ({ client, message, say }) => {
+  app.message(/[Gg]orani [Bb]attle|고라니 배틀/, async ({client, message, say}) => {
     // @ts-expect-error
     const opponent = message.text.split(/[Gg]orani [Bb]attle |고라니 배틀 /)[1];
 
     const {
       // @ts-expect-error
-      user: { real_name },
+      user: {real_name},
       // @ts-expect-error
-    } = await client.users.info({ user: message.user });
+    } = await client.users.info({user: message.user});
 
     const winner = Math.floor(Math.random() * 2);
     const winMessage = randomMessage(winner, real_name, opponent);
 
     await say(winMessage);
   });
+
+  app.message(/[Gg]orani [Bb]irthday|고라니 생일/, async ({say}) => {
+    // @ts-expect-error
+    await say({
+      blocks: [{
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: '```' +
+            '김소현: 3월 2일\n' +
+            '홍승현: 3월 16일\n' +
+            '김한솔: 4월 21일\n' +
+            '이주호: 10월 31일\n' +
+            '유예빈: 11월 2일\n' +
+            '고승화: 11월 3일\n' +
+            '김진수: 11월 21일\n' +
+            '```'
+        }
+      }]
+    })
+  })
 }
 
 export default addBasicEvent;
